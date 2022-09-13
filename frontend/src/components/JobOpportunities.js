398ba17a2be00909780b02e42bc7675ff5325400
Ajay-Ganapathy <82525751+Ajay-Ganapathy@users.noreply.github.com>
2022-09-13 10:43:15 +0530

    Add files via upload

---
+++ frontend/src/components/JobOpportunities.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobOpportunities = () => {
+  return (
+    <div>JobOpportunities</div>
+  )
+}
+
+export default JobOpportunities
---
+++ frontend/src/components/StarAlumni.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StarAlumni = () => {
+  return (
+    <div>StarAlumni</div>
+  )
+}
+
+export default StarAlumni
