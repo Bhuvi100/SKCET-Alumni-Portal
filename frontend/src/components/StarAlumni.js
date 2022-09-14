45415e459f20a67b1dc651253c5ebebeb29059b5
Ajay-Ganapathy <82525751+Ajay-Ganapathy@users.noreply.github.com>
2022-09-14 11:41:45 +0530

    Update StarAlumni.js

--- frontend/src/components/StarAlumni.js
+++ frontend/src/components/StarAlumni.js
@@ -1,8 +1,78 @@
 import React from 'react'
-
 const StarAlumni = () => {
   return (
-    <div>StarAlumni</div>
+    <div id={"initiatives"}>
+      <section className="text-black mt-12">
+        <div class="max-w-screen-xl px-4 py-15 mx-auto sm:px-6 lg:px-8">
+          <div class="max-w-lg mx-auto text-center">
+           
+            <h1 className={" mb-16 pb-2 font-bold sm:text-xl text-2xl md:text-3xl text-center"}>
+              Notable
+              <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+                Alumni
+              </span>
+              <div className=""></div>
+            </h1>
+          </div>
+
+          <div className="grid grid-cols-1 gap-8 mt-12 md:grid-cols-2 lg:grid-cols-3">
+            <div className={" block p-8 transition shadow-xl rounded-xl hover:shadow-pink-500/10 hover:border-pink-500/10"}>
+             
+            <img className="m-auto" src="https://skcet.ac.in/images/testimonials.jpg" alt="First slide" style={{'width':'220px','height':'210px'}} />
+              <h3 className="mt-4 text-xl font-bold text-center text-indblue">
+               Working at Google
+              </h3>
+              <div className="m-4" >
+              <a href="https://ariia.gov.in/" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  px-4 rounded' target="_blank" >View Profile</a>
+              </div>
+            </div>
+
+            <div className={" block p-8 transition shadow-xl rounded-xl hover:shadow-pink-500/10 hover:border-pink-500/10"}>
+             
+             <img className="m-auto" src="https://skcet.ac.in/images/testimonials.jpg" alt="First slide" style={{'width':'220px','height':'210px'}} />
+               <h3 className="mt-4 text-xl font-bold text-center text-indblue">
+                Working at Google
+               </h3>
+               <div className="m-4">
+               <a href="https://ariia.gov.in/" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  px-4 rounded' target="_blank" >View Profile</a>
+               </div>
+             </div>
+             <div className={" block p-8 transition shadow-xl rounded-xl hover:shadow-pink-500/10 hover:border-pink-500/10"}>
+             
+             <img className="m-auto" src="https://skcet.ac.in/images/testimonials.jpg" alt="First slide" style={{'width':'220px','height':'210px'}} />
+               <h3 className="mt-4 text-xl font-bold text-center text-indblue">
+                Working at Google
+               </h3>
+               <div className="m-4">
+               <a href="https://ariia.gov.in/" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  px-4 rounded' target="_blank" >View Profile</a>
+               </div>
+             </div>
+             <div className={" block p-8 transition shadow-xl rounded-xl hover:shadow-pink-500/10 hover:border-pink-500/10"}>
+             
+             <img className="m-auto" src="https://skcet.ac.in/images/testimonials.jpg" alt="First slide" style={{'width':'220px','height':'210px'}} />
+               <h3 className="mt-4 text-xl font-bold text-center text-indblue">
+                Working at Google
+               </h3>
+               <div className="m-4">
+               <a href="https://ariia.gov.in/" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  px-4 rounded' target="_blank" >View Profile</a>
+               </div>
+             </div>
+             <div className={" block p-8 transition shadow-xl rounded-xl hover:shadow-pink-500/10 hover:border-pink-500/10"}>
+             
+             <img className=" " src="https://skcet.ac.in/images/testimonials.jpg" alt="First slide" style={{'width':'220px','height':'210px'}} />
+               <h3 className="mt-4 text-xl font-bold text-center text-indblue">
+                Working at Google
+               </h3>
+               <div className="m-4" >
+                <a href="https://ariia.gov.in/" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  px-4 rounded' target="_blank" >View Profile</a>
+               </div>
+             </div>
+
+          </div>
+          
+        </div>
+      </section>
+    </div>
   )
 }
 
