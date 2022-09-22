03d172dc269fdb14b7cfe03828ddba3d3e153249
ashwinbiju09 <20eucs018@skcet.ac.in>
2022-09-22 22:44:17 +0530

    Updates

--- frontend/src/components/QueriesForm.js
+++ frontend/src/components/QueriesForm.js
@@ -6,48 +6,38 @@ const QueriesForm = () => {
   
 
   return (
-    <div id="bg">
-  
-    <div class="max-w-2xl  mx-auto bg-slate-200 p-5 mt-5">
-  <div class="flex flex-col justify-center items-center h-12 text-3xl font-extrabold bg-blue-600 mb-2 mt-5" style = {{display : "flex", justifyContent : "center" ,alignItems : "center"}}>
-       <h3 class="text-lg text-white">General Queries</h3>
-     </div>
- <form >
-       
- 
-   <div >
-       <div>
-           <label for="Full Name" class="block mb-2 text-black font-bold"> Name</label>
-           <input type="text" class="bg-white border border-gray-300 text-gray-900 text-medium rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
-       </div>
-       
-        <div>
-           <label for="Mob" class="block mb-2 text-black font-bold">Mobile Number</label>
-           <input type="tel" class="bg-white border border-gray-300 text-gray-900 text-medium rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
-       </div>
-       <div>
-           <label for="email" class="block mb-2 text-black font-bold">Subject</label>
-           <input type="email" class="bg-white border border-gray-300 text-gray-900 text-medium rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
-       </div>
-        <div>
-           <label for="job" class="block mb-2 text-black font-bold">Query</label>
-           <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 mb-3 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Post your queries..."></textarea>
-       </div>
+<div id="bg" class="pt-7">
+   <div class="max-w-2xl  mx-auto bg-white p-5 rounded-lg shadow-inner border-solid border-2 border-gray-100">
+      <div class="font-lg mb-2 mt-5" style = {{display : "flex", justifyContent : "center" ,alignItems : "center"}}>
+      <h4 class="text-2xl font-semibold mt-4 mb-4 pb-1">GENERAL
+         <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-blue-300">QUERIES</span>
+      </h4>
+   </div>
+   <form >
+      <div >
+         <div class="mt-4 mb-4">
+            <label for="Full Name" class="block mb-2 text-black dark:text-gray-900"> Name</label>
+            <input type="text" class="block w-full px-4 py-2 mt-2  bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring" required/>
+         </div>
+         <div class="mt-4 mb-4">
+            <label for="Mob" class="block mb-2 text-black dark:text-gray-900">Mobile Number</label>
+            <input type="tel" class="block w-full px-4 py-2 mt-2  bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring" required/>
+         </div>
+         <div class="mt-4 mb-4">
+            <label for="email" class="block mb-2 text-black dark:text-gray-900">Subject</label>
+            <input type="email" class="block w-full px-4 py-2 mt-2  bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring" required/>
+         </div>
+         <div class="mt-4 mb-4">
+            <label for="job" class="block mb-2 text-black dark:text-gray-900">Query</label>
+            <textarea id="message" rows="4" class="block w-full px-4 py-2 mt-2  bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring" placeholder="Post your queries..."></textarea>
+         </div>
       </div>
-
-       
-      
-       <div>
-       
-     
-       </div>
-       
-   <div class="flex items-center justify-center">
-   <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-   </div>
-</form>
+      <div class="flex items-center justify-center">
+         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm w-full sm:w-auto px-6 py-2.5 text-center" style={{ background: "linear-gradient(to right, #2A2A72, #009FFD)" }}>SUBMIT</button>
+      </div>
+   </form>
 </div>
-   </div>
+</div>
   )
 }
 
