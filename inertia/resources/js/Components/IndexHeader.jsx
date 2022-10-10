a7268acafcfabd9a07b555b5cfe61f13711c9546
Ajay-Ganapathy <82525751+Ajay-Ganapathy@users.noreply.github.com>
2022-10-10 13:04:26 +0530

    Update IndexHeader.jsx

--- inertia/resources/js/Components/IndexHeader.jsx
+++ inertia/resources/js/Components/IndexHeader.jsx
@@ -1,100 +1,104 @@
 import React, { useState , useEffect } from 'react';
 import {Link } from "react-router-dom"
+import "../App.css"
 import Navbar from './Navbar';
-import "../../css/app.css"
-
-import design from '../../../public/assets/design.png';
-import Chairperson from '../../../public/assets/Chairperson.jpg';
-import Principal from '../../../public/assets/Principal.jpg';
+/*import UntitledDesign from './../assets/Untitled design.png';
+import Chairperson from './../assets/Chairperson.jpg';
+import Principal from './../assets/Principal.jpg';*/
 
 
 const IndexHeader = () => {
   return (
-  
     <div class="leading-normal tracking-normal text-white gradient" style={{ background: "linear-gradient(to right, #2A2A72, #009FFD)", fontFamily:"sans-serif" }}>
     <Navbar />
-    <div class="pt-24">
-      <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="px-24 mb-11">
+    <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center justify-between flex-end" >
         <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-          <p class="uppercase tracking-loose w-full">Lorem Ipsum</p>
-          <h1 class="my-4 text-5xl font-bold leading-tight">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          <p class="text-3xl tracking-loose w-full text-gray-100 font-bold rounded-full tracking-wider">Relive & Reconnect</p>
+          <h1 class="my-4 text-5xl font-bold leading-tight ">
+            Welcome to the Alumni Network of Sri Krishna College Of Engineering & Technology
           </h1>
-          <p class="leading-normal text-2xl mb-8">
+          <p class="leading-normal text-2xl tracking-loose w-full">
             Not a part of our community yet ?
           </p>
-          <a href = "/register" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+          <Link to = "/register" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
             Join Today 
-          </a>
+          </Link>
         </div>
-        <div class="w-full md:w-3/5 py-6 text-center">
-          <img class="w-full md:w-4/5 z-50 p-10 ml-0 md:ml-40 " src={design} />
+        <div class="w-46 mt-7 mb-10">
+          <img class="w-full md:w-3/4 z-50  ml-0 object-center" style = {{display : "flex", justifyContent : "center", alignItems : "center"}} src="/assets/hero.png" />
         </div>
       </div>
     </div>
-    <div class="relative -mt-12 lg:-mt-24">
-      <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
-            <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
-            <path
-              d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
-              opacity="0.100000001"
-            ></path>
-            <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path>
-          </g>
-          <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
-            <path
-              d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"
-            ></path>
-          </g>
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="relative -mt-20 lg:-mt-24">
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+        <defs>
+          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g class="parallax">
+          <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
+          <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+          <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+          <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
         </g>
       </svg>
     </div>
 
-    <section class="bg-white border-b py-8 place-items-center">
-      <div class="container max-w-5xl mx-auto m-8 content-center">
-      <div class="max-w-lg mx-auto text-center">
-           <h1 className=" mb-16 pb-2 font-bold sm:text-xl text-2xl md:text-3xl text-center text-black">
-             SKCET
-             <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-blue-300" >
-               LinkUP
-             </span>
-           </h1>
-         </div>
-        <div class="flex flex-wrap">
-          <div class="w-5/6 sm:w-1/2 p-6">
-            <h3 class="text-gray-600 ml-10 mb-8">
-               To produce Globally competitive Engineers with Social Responsibilities and High Ethical Values.
-            </h3>
-            <p class="text-3xl text-gray-800 font-bold leading-none mb-3 ml-10">
-                Smt. S Malarvizhi , Chairperson
-              <br />
-              <br />
-            </p>
+
+    <section class="about" className='bg-white '>
+    <div class="text-gray-900 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+            <p class="leading-relaxed text-lg">We ventured to believe that the bond that former Skcetians had with their professors and friends would strengthen and become much deeper over time.SKCET LinkUP has been given new life with the purpose of increasing the influence of the Skcet Alumni Community on many more lives.Former Skcetians will now get information about the Institution's triumphs, their fellow Skcetians' accomplishments, new life milestones, industry insights, employment opportunities, exciting learning opportunities, and much more.</p>
           </div>
-         <div class="w-full sm:w-1/2 p-6">
-            <img class=" object-fill h-48 w-48 rounded-full shadow-2xl ml-20"   src= {Chairperson} />
-          </div> 
         </div>
-        <br />
-        <div class="flex flex-wrap">
-            <div class="w-5/6 sm:w-1/2 p-6">
-              <h3 class="text-gray-600 ml-10 mb-8">
-                To focus on educational stewardship and create innovative connections in engineering and technology with unparalleled student learning experiences of Global significance.
-            </h3>
-              <p class="text-3xl text-gray-800 font-bold leading-none mb-3 ml-10">
-                Smt. J. Janet ,Principal
-                <br />
-                <br />
-              </p>
+      </div>
+    <div class="text-gray-800 body-font">
+        <div class="container px-5 mx-auto">
+          <div class="flex flex-wrap m-4">
+            <div class="p-4 md:w-1/2 w-full">
+              <div class="h-full bg-gray-100 p-8 rounded">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#000" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                  <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                </svg>
+                <p class="leading-relaxed mb-6">To produce Globally competitive Engineers with Social Responsibilities and High Ethical Values.</p>
+                <div class="inline-flex items-center">
+                  <img alt="testimonial" src="/assets/Chairperson.jpg" class="w-36 h-36 rounded-full flex-shrink-0  object-center" />
+                  <span class="flex-grow flex flex-col pl-4">
+                    <span class="title-font font-medium text-gray-900">Smt. S Malarvizhi , Chairperson</span>
+                    <span class="text-gray-500 text-sm">Sri Krishna Institutions</span>
+                  </span>
+                </div>
+              </div>
             </div>
-             <div class="w-full sm:w-1/2 p-6">
-              <img class="object-fill h-48 w-48 rounded-full shadow-2xl ml-20"  src={Principal} />
-            </div> 
+            <div class="p-4 md:w-1/2 w-full">
+              <div class="h-full bg-gray-100 p-8 rounded">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#000" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                  <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                </svg>
+                <p class="leading-relaxed mb-6">To focus on educational stewardship and create innovative connections in engineering and technology with unparalleled student learning experiences of Global significance.</p>
+                <div class="inline-flex items-center">
+                  <img alt="testimonial" src="/assets/Principal.jpg" class="w-36 h-36 rounded-full flex-shrink-0 object-center" />
+                  <span class="flex-grow flex flex-col pl-4">
+                    <span class="title-font font-medium text-gray-900 ">Smt. J. Janet , Principal</span>
+                    <span class="text-gray-500 text-sm">Sri Krishna College Of Engineering & Technology</span>
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-    </section>
+    </div>
+    <br />
+  </section>
   </div>
   )
 }
