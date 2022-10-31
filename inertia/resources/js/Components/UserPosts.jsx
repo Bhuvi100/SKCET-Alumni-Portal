255d45170f9d01a505755790934884c4aef9dca8
Balamithran228 <ssbalabhavani228@gmail.com>
2022-10-31 12:07:47 +0530

    added comment function

--- inertia/resources/js/Components/UserPosts.jsx
+++ inertia/resources/js/Components/UserPosts.jsx
@@ -11,6 +11,7 @@ const UserPosts = (props) => {
 
 
   const [comment, setComment] = useState([]);
+
   const [postdata, setPostdata] = useState([]);
   const [pid,setPid] = useState(0)
   const [com,setCom] = useState(false)
@@ -22,6 +23,21 @@ const UserPosts = (props) => {
   const [dtitle, setDtitle] = useState("")
   const [ddescription, setDdescription] = useState("")
 
+  const { addComment, setAddComment, errors, post } = useForm({
+    comment_id: null,
+    description: "",
+ });
+ 
+ function handleSubmitCommentsAndReply(e) {
+    e.preventDefault();
+    post(route("addComment",`${postid}`, {
+      onSuccess: (res) => {
+        console.log(
+          "sucessfull"
+        );
+      }
+    }));
+ }
 
   useEffect(() => {
     axios().get("http://127.0.0.1:8000/Posts").then((response) => {
@@ -30,6 +46,7 @@ const UserPosts = (props) => {
       
     });
   }, []);
+
 
  
 
@@ -46,6 +63,8 @@ const GetComments = (post_id) => {
     });
     console.log(post_id);
   }
+
+  
   return (
     <div>
      
@@ -183,21 +202,22 @@ const GetComments = (post_id) => {
     </div>
 </section>
                   )) 
-}
-                  <div class="relative flex items-center self-center w-full max-w-xl p-4 overflow-hidden text-gray-600 focus-within:text-gray-400">
-                      <img class="w-10 h-10 object-cover rounded-full shadow mr-2 cursor-pointer" alt="User avatar" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2000&amp;q=80"/>
-                      <span class="absolute inset-y-0 right-0 flex items-center pr-6">
-                          <button type="submit" class="p-1 focus:outline-none focus:shadow-none hover:text-blue-500">
-                          <svg class="w-6 h-6 transition ease-out duration-300 hover:text-blue-500 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                          </svg>
-  
-                          </button>
-                      </span>
-                     
-                       <input type="search" class="w-full py-2 pl-4 pr-10 text-sm bg-gray-100 border border-transparent appearance-none rounded-tg placeholder-gray-400 focus:bg-white focus:outline-none focus:border-blue-500 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Post a comment..." autocomplete="off"/>
-                     
-                  </div>
+}                  <form onSubmit={handleSubmit} >
+                                <div class="relative flex items-center self-center w-full max-w-xl p-4 overflow-hidden text-gray-600 focus-within:text-gray-400">
+                                    <img class="w-10 h-10 object-cover rounded-full shadow mr-2 cursor-pointer" alt="User avatar" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2000&amp;q=80"/>
+                                    <span class="absolute inset-y-0 right-0 flex items-center pr-6">
+                                        <button type="submit" class="p-1 focus:outline-none focus:shadow-none hover:text-blue-500">
+                                        <svg class="w-6 h-6 transition ease-out duration-300 hover:text-blue-500 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                        </svg>
+                
+                                        </button>
+                                    </span>
+                                    
+                                    <input type="text" class="w-full py-2 pl-4 pr-10 text-sm bg-gray-100 border border-transparent appearance-none rounded-tg placeholder-gray-400 focus:bg-white focus:outline-none focus:border-blue-500 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Post a comment..." autocomplete="off"/>
+                                    
+                                </div>
+                  </form>
               </div>
         ))
               }
