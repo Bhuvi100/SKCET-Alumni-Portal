e5b8d3b4d023b3c61b4a5d03f9864e998bcf855d
Bhuvanesh T G <gbhuvanesh100@gmail.com>
2022-11-03 12:15:11 +0530

    Update seeder

--- inertia/database/seeders/DatabaseSeeder.php
+++ inertia/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
              'name' => 'Test User',
              'email' => 'test@example.com',
              'phone' => '1234567899',
-             'password' => 'password'
+             'password' => \Hash::make('password')
          ]);
     }
 }
