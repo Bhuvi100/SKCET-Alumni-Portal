e2cbf9c3aef81467647acef145b4a438a8e80e23
Bhuvanesh T G <gbhuvanesh100@gmail.com>
2022-11-12 23:52:19 +0530

    Update

--- laravel8/resources/js/Layouts/AuthenticatedLayout.jsx
+++ laravel8/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import SideBar from "../Components/SideBar";
 import HomeNav from "../Components/HomeNav";
+import { useEffect } from "react";
+import { Head } from "@inertiajs/inertia-react";
 
 export default function Authenticated({ auth, header, children }) {
+    useEffect(() => console.log("awsdeas"), []);
 
     return (
         <div className="bg-gray-300">
+            {header && (
+                <Head>
+                    <title>{header}</title>
+                </Head>
+            )}
             <div className="flex">
-                <HomeNav auth={auth}/>
-                
-                {
-                    window.innerWidth>700 && <SideBar />
-                }
-                <main className="w-full m-3 md:ml-80  mt-24" >
-                    {children}
-                </main>
-               
-                
+                <HomeNav auth={auth} />
+                {window.innerWidth > 700 && <SideBar />}
+                <main className="w-full m-3 md:ml-80  mt-24">{children}</main>A
             </div>
         </div>
     );
