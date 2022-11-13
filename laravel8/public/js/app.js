1e62f37c767339f10c455be65213caaf601783ac
Ajay-Ganapathy <20epci002@skcet.ac.in>
2022-11-13 09:02:18 +0530

    fix chapters

--- laravel8/public/js/app.js
+++ laravel8/public/js/app.js
@@ -82899,31 +82899,31 @@ function Dashboard(props) {
       title: "Chapters"
     }), /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("div", {
       children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("div", {
-        className: "flex  flex-col flex-1",
+        className: "flex flex-row flex-1",
         children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("main", {
           className: "pb-16 overflow-y-auto mt-16",
           children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("div", {
-            className: "block grid-cols-1 grid-rows-1 md:grid md:grid-flow-row-dense md:grid-cols-3  md:grid-rows-3 ",
+            className: "block md:grid md:grid-flow-row-dense md:grid-cols-3 grid-cols-1 grid-rows-1 md:grid-rows-3 gap-10",
             children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("div", {
               className: "col-span-2",
               children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsxs"])("div", {
-                className: "flex flex-row mt-5 justify-center",
+                className: "flex flex-col md:flex-row mt-5 justify-center",
                 children: [/*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("div", {
                   className: "container mr-4 mt-10 px-6 text-gray-500 md:px-12 xl:px-0 mb-5 ",
                   children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsxs"])("div", {
                     className: "max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white",
                     children: [/*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("img", {
-                      className: "w-full",
-                      src: "/assets/ski_logo.png",
-                      alt: "Sunset in the mountains"
+                      className: "w-full h-4/5",
+                      src: "/assets/Coimbatore.jpg",
+                      alt: "Coimbatore Chapter"
                     }), /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsxs"])("div", {
                       className: "px-6 py-4",
                       children: [/*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("div", {
                         className: "font-bold text-xl mb-2",
                         children: "Coimbatore Chapter"
                       }), /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("p", {
-                        className: "text-gray-700 text-base",
-                        children: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+                        className: "text-gray-700 text-xl",
+                        children: "Being the Hometown of Sri Krishna Family, Coimbatore chapter plays a major role in SKCET's Alumni Network."
                       })]
                     })]
                   })
@@ -82932,17 +82932,17 @@ function Dashboard(props) {
                   children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsxs"])("div", {
                     className: "max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white",
                     children: [/*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("img", {
-                      className: "w-full",
-                      src: "/assets/ski_logo.png",
-                      alt: "Sunset in the mountains"
+                      className: "w-full h-4/5",
+                      src: "/assets/Chennai.jpeg",
+                      alt: "Chennai Chapter"
                     }), /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsxs"])("div", {
                       className: "px-6 py-4",
                       children: [/*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("div", {
                         className: "font-bold text-xl mb-2",
                         children: "Chennai Chapter"
                       }), /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("p", {
-                        className: "text-gray-700 text-base",
-                        children: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+                        className: "text-gray-700 text-xl",
+                        children: "As the Capital city and the largest city of Tamilnadu, Chennai chapter is dedicated for those who are living in Chennai."
                       })]
                     })]
                   })
@@ -82951,17 +82951,17 @@ function Dashboard(props) {
                   children: /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsxs"])("div", {
                     className: "max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white",
                     children: [/*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("img", {
-                      className: "w-full",
-                      src: "/assets/ski_logo.png",
-                      alt: "Sunset in the mountains"
+                      className: "w-full h-4/5",
+                      src: "/assets/Bangalore.jpeg",
+                      alt: "Bangalore Chapter"
                     }), /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsxs"])("div", {
                       className: "px-6 py-4",
                       children: [/*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("div", {
                         className: "font-bold text-xl mb-2",
                         children: "Bangalore Chapter"
                       }), /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__["jsx"])("p", {
-                        className: "text-gray-700 text-base",
-                        children: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+                        className: "text-gray-700 text-xl",
+                        children: "As the center of India's high-tech industry, Bangalore Chapter is catered to alumni residing in Bangalore."
                       })]
                     })]
                   })
