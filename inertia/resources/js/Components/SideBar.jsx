3c8dda45e526121917acc22fc64df476c5136c40
Ajay-Ganapathy <20epci002@skcet.ac.in>
2023-08-03 11:58:50 +0530

    update chapters from laravel8 to Inertia

--- inertia/resources/js/Components/SideBar.jsx
+++ inertia/resources/js/Components/SideBar.jsx
@@ -23,11 +23,11 @@ export default function SideBar(props) {
                                 stroke="currentColor"
                                 class="w-6 h-6"
                             >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                />
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                                    />
                             </svg>
                         </button>
                     )}
--- inertia/resources/js/Pages/Chapters.jsx
+++ inertia/resources/js/Pages/Chapters.jsx
@@ -1,8 +1,8 @@
 import { Head } from "@inertiajs/inertia-react";
-import Posts from "@/Components/Posts";
-import TrendingPosts from "@/Components/TrendingPosts";
-import UserPosts from "@/Components/UserPosts";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import Posts from "../Components/Posts";
+import TrendingPosts from "../Components/TrendingPosts";
+import UserPosts from "../Components/UserPosts";
+import AuthenticatedLayout from "../Layouts/AuthenticatedLayout";
 
 export default function Dashboard(props) {
     return (
@@ -17,33 +17,28 @@ export default function Dashboard(props) {
         >
             <Head title="Chapters" />
             <div>
-                <div className="flex flex-col flex-1">
-                    <main className="pb-16 overflow-y-auto">
-                        <div className="block md:grid md:grid-flow-row-dense md:grid-cols-3 md:grid-rows-3 ">
+                <div className="flex flex-row flex-1">
+                    <main className="pb-16 overflow-y-auto mt-16">
+                        <div className="block md:grid md:grid-flow-row-dense md:grid-cols-3 grid-cols-1 grid-rows-1 md:grid-rows-3 gap-10">
                             <div className="col-span-2">
-                                <div className="flex flex-row mt-5 justify-center">
+                                <div className="flex flex-col md:flex-row mt-5 justify-center">
                                     <div className="container mr-4 mt-10 px-6 text-gray-500 md:px-12 xl:px-0 mb-5 ">
                                         <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white">
                                             <img
-                                                className="w-full"
-                                                src="/assets/ski_logo.png"
-                                                alt="Sunset in the mountains"
+                                                className="w-full h-4/5"
+                                                src="/assets/Coimbatore.jpg"
+                                                alt="Coimbatore Chapter"
                                             />
                                             <div className="px-6 py-4">
                                                 <div className="font-bold text-xl mb-2">
                                                     Coimbatore Chapter
                                                 </div>
-                                                <p className="text-gray-700 text-base">
-                                                    Lorem ipsum dolor sit amet,
-                                                    consectetur adipisicing
-                                                    elit. Voluptatibus quia,
-                                                    nulla! Maiores et
-                                                    perferendis eaque,
-                                                    exercitationem praesentium
-                                                    nihil.
+                                                <p className="text-gray-700 text-xl">
+                                                    Being the Hometown of Sri
+                                                    Krishna Family, Coimbatore
+                                                    chapter plays a major role
+                                                    in SKCET's Alumni Network.
                                                 </p>
-
-                                                <button className = "mt-3 btn btn-primary">Join Now</button>
                                             </div>
                                             {/*<div className="px-6 pt-4 pb-2">*/}
                                             {/*    <span*/}
@@ -58,24 +53,21 @@ export default function Dashboard(props) {
                                     <div className="container mr-4 mt-10 px-6 text-gray-500 md:px-12 xl:px-0 mb-5 ">
                                         <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white">
                                             <img
-                                                className="w-full"
-                                                src="/assets/ski_logo.png"
-                                                alt="Sunset in the mountains"
+                                                className="w-full h-4/5"
+                                                src="/assets/Chennai.jpeg"
+                                                alt="Chennai Chapter"
                                             />
                                             <div className="px-6 py-4">
                                                 <div className="font-bold text-xl mb-2">
                                                     Chennai Chapter
                                                 </div>
-                                                <p className="text-gray-700 text-base">
-                                                    Lorem ipsum dolor sit amet,
-                                                    consectetur adipisicing
-                                                    elit. Voluptatibus quia,
-                                                    nulla! Maiores et
-                                                    perferendis eaque,
-                                                    exercitationem praesentium
-                                                    nihil.
+                                                <p className="text-gray-700 text-xl">
+                                                    As the Capital city and the
+                                                    largest city of Tamilnadu,
+                                                    Chennai chapter is dedicated
+                                                    for those who are living in
+                                                    Chennai.
                                                 </p>
-                                                <button className = "mt-3 btn btn-primary">Join Now</button>
                                             </div>
                                             {/*<div className="px-6 pt-4 pb-2">*/}
                                             {/*    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">*/}
@@ -93,24 +85,21 @@ export default function Dashboard(props) {
                                     <div className="container mt-10 px-6 text-gray-500 md:px-12 xl:px-0 mb-5 ">
                                         <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white">
                                             <img
-                                                className="w-full"
-                                                src="/assets/ski_logo.png"
-                                                alt="Sunset in the mountains"
+                                                className="w-full h-4/5"
+                                                src="/assets/Bangalore.jpeg"
+                                                alt="Bangalore Chapter"
                                             />
                                             <div className="px-6 py-4">
                                                 <div className="font-bold text-xl mb-2">
                                                     Bangalore Chapter
                                                 </div>
-                                                <p className="text-gray-700 text-base">
-                                                    Lorem ipsum dolor sit amet,
-                                                    consectetur adipisicing
-                                                    elit. Voluptatibus quia,
-                                                    nulla! Maiores et
-                                                    perferendis eaque,
-                                                    exercitationem praesentium
-                                                    nihil.
+                                                <p className="text-gray-700 text-xl">
+                                                    As the center of India's
+                                                    high-tech industry,
+                                                    Bangalore Chapter is catered
+                                                    to alumni residing in
+                                                    Bangalore.
                                                 </p>
-                                                <button className = "mt-3 btn btn-primary">Join Now</button>
                                             </div>
                                             {/*<div className="px-6 pt-4 pb-2">*/}
                                             {/*    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">*/}
